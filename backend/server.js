be87fbd852a1d4a7c416b0c712af9e0a9f858874
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-08 11:37:09 -0800

    true routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 const app = express();
 
@@ -11,9 +12,8 @@ app.use((req, res, next) => {
 })
 
 // Routes
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the app' })
-})
+app.use('/api/workouts', workoutRoutes)
+
 
 // listen for requests
 app.listen(process.env.PORT, () => {
