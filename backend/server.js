710c480289df6516cc70b0523100c2e9862e691e
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-08 12:10:39 -0800

    setting up mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 const app = express();
@@ -15,6 +16,13 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/api/workouts', workoutRoutes)
+
+// connect to db
+mongoose.connect(process.env.DATABASE_URL)
+.then(() => {})
+.catch((error) => {
+    console.log(error)
+})
 
 
 // listen for requests
