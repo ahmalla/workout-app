12f0245a6c612847b73cbc30365cbb325f778469
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-08 13:33:19 -0800

    added pages folder and home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
 
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <BrowserRouter>
+      <div className='pages'>
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+          />
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
