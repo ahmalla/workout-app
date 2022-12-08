7da4819639d3ac09b0cd2f75a07ad3eb94a8eed9
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-08 14:45:15 -0800

    context added

---
+++ frontend/src/context/WorkoutContext.js
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+export const WorkoutsContext = createContext()
+
+export const WorkoutsContextProvider = ({ children }) => {
+
+    return (
+        <WorkoutsContextProvider>
+            { children }
+        </WorkoutsContextProvider>
+    )
+}
+
+// 
