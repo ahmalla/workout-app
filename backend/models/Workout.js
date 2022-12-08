dbcd295920d12e55ef10e5dbbf194e539356b4f4
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-08 12:21:13 -0800

    workout schema exported

--- backend/models/Workout.js
+++ backend/models/Workout.js
@@ -9,5 +9,11 @@ const workoutSchema = new Schema({
     reps: {
         type: Number,
         required: true
+    },
+    load: {
+        type: Number,
+        required: true
     }
-})
+}, { timestamps: true })
+
+module.exports = mongoose.model('Workout', workoutSchema)
