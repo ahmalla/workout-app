97e02094d40d52cfc545bc4985cd51c6385bafaf
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-08 18:04:51 -0800

    setup controller functions

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+// login user
+const loginUser = async (req, res) => {
+    res.json({message: 'login user'})
+}
+
+// signup user
+const signupUser = async (req, res) => {
+    res.json({message: 'signup user'})
+}
+
+module.exports = { signupUser, loginUser }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
-
 const router = express.Router()
+
+// controller functions
 
 // Login route
 router.post('/login', () => {})
