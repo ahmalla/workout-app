d8a2e9978cdec578adf32c4bdecbfb3e997513d1
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-08 16:34:22 -0800

    delete workouts

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,7 +1,17 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+
 const WorkoutDetails = ({ workout }) => {
+    const { dispatch } = useWorkoutsContext()
 
 const handleClick = async () => {
+    const response = await fetch('/api/workouts/' + workout._id, {
+        method: 'DELETE'
+    })
+    const json = await response.json()
 
+    if (response.ok) {
+        dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
 }
 
     return (
