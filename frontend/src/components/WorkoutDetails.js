557705d1ab2d477fb600a5ac41743cf0c96d2d31
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-08 16:21:23 -0800

    handle click

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,10 +1,16 @@
 const WorkoutDetails = ({ workout }) => {
+
+const handleClick = async () => {
+
+}
+
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Load (lbs): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>Delete</span>
         </div>
     )
 }
