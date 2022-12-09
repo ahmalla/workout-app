bb57f7901a06c6de49a60698953434685a1e23c6
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-08 17:56:23 -0800

    user routes setup

---
+++ backend/routes/user.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const router = express.Router()
+
+// Login route
+router.post('/login', () => {})
+
+// Signup route
+router.post('/signup', () => {})
+
+
+
+
+
+module.exports = router
