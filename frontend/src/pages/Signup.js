f1cc144d0422ed8d857a45bd9f2a3a7729a8796f
ahmalla <68600561+ahmalla@users.noreply.github.com>
2022-12-08 20:22:42 -0800

    Auth

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,13 +1,15 @@
-const { useState } = require("react")
+import { useState} from 'react'
+import { useSignup } from '../hooks/useSignup'
 
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const {signup, error, isLoading} = useSignup()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email, password)
+        await signup(email, password)
 
     }
 
@@ -28,7 +30,8 @@ const Signup = () => {
                 value={password}
             />
 
-            <button>Sign Up</button>
+            <button disabled={isLoading}>Sign Up</button>
+            {error && <div className='error'></div>}
         </form>
     )
 }
